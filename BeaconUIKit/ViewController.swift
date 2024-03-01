0bec2973671d4f140f10d74aeab9d390467b2c21
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-03-01 15:07:12 -0600

    Initial commit

--- BeaconUIKit/ViewController.swift
+++ BeaconUIKit/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+	
+	// MARK: - VIEW PROPERTIES
+	var locationManager: CLLocationManager?
+	
+	
+	// MARK: - @IBOUTLET
+	@IBOutlet var distanceReading: UILabel!
+	
+	
+	// MARK: - VIEWDIDLOAD
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		
+		locationManager = CLLocationManager()
+		locationManager?.delegate = self
+		locationManager?.requestAlwaysAuthorization()
+		
+		view.backgroundColor = .gray
 	}
-
-
+	
+	// MARK: - @OBJC FUNCTIONS
+	@objc func something() {
+		//
+	}
+	
+	// MARK: - FUNCTIONS
+	func somethingElse() {
+		//
+	}
+	
+	func locationManager(
+		_ manager: CLLocationManager,
+		didChangeAuthorization status: CLAuthorizationStatus
+	) {
+		if status == .authorizedAlways {
+			// IS MONITORING AVAILABLE? If yes, then check if Ranging is Available
+			if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+				if CLLocationManager.isRangingAvailable() {
+					// do stuff
+				}
+			}
+		}
+	}
 }
+
+
 
