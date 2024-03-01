f1659f1734bcf334008037d6e4030189a8444fbf
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-03-01 15:49:18 -0600

    Beacon all the things

--- BeaconUIKit/ViewController.swift
+++ BeaconUIKit/ViewController.swift
@@ -31,15 +31,21 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 	}
 	
 	// MARK: - @OBJC FUNCTIONS
+	/// Blank @objc method
 	@objc func something() {
 		//
 	}
 	
 	// MARK: - FUNCTIONS
+	/// Blank function
 	func somethingElse() {
 		//
 	}
 	
+	/// Location Management Function
+	/// - Parameters:
+	///   - manager: CoreLocation Manager
+	///   - status: is set to Authorized Always
 	func locationManager(
 		_ manager: CLLocationManager,
 		didChangeAuthorization status: CLAuthorizationStatus
@@ -48,9 +54,53 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 			// IS MONITORING AVAILABLE? If yes, then check if Ranging is Available
 			if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
 				if CLLocationManager.isRangingAvailable() {
-					// do stuff
+					startScanning()
 				}
 			}
+		}
+	}
+	
+	/// Sets UUID for scanning beacons
+	func startScanning() {
+		let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+		let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+		
+		locationManager?.startMonitoring(for: beaconRegion)
+		locationManager?.startRangingBeacons(in: beaconRegion)
+	}
+	
+	func update(distance: CLProximity) {
+		UIView.animate(withDuration: 1) {
+			switch distance {
+			
+			case .far:
+				self.view.backgroundColor = .blue
+				self.distanceReading.text = "FAR"
+				
+			case .near:
+				self.view.backgroundColor = .orange
+				self.distanceReading.text = "NEAR"
+				
+			case .immediate:
+				self.view.backgroundColor = .red
+				self.distanceReading.text = "RIGHT HERE"
+				
+			default:
+				self.view.backgroundColor = .gray
+				self.distanceReading.text = "UNKOWN"
+			}
+		}
+	}
+	
+	func locationManager(
+		_ manager: CLLocationManager,
+		didRangeBeacons beacons: [CLBeacon],
+		in region: CLBeaconRegion
+	) {
+		if let beacon = beacons.first {
+			update(distance: beacon.proximity)
+		} else {
+			update(distance: .unknown)
 		}
 	}
 }
